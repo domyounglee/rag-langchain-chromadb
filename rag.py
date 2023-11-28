22d077bb825fee98a6d1fc62131212139a117220
Joseph <j16065682@gmail.com>
2023-11-28 17:15:10 +0900

    add compressor

--- rag.py
+++ rag.py
@@ -6,9 +6,23 @@ from langchain.llms import HuggingFacePipeline
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
-from langchain.prompts import PromptTemplate
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_transformers import EmbeddingsRedundantFilter
 
+
+# Wrap our vectorstore
+from langchain.retrievers.document_compressors import DocumentCompressorPipeline,EmbeddingsFilter, LLMChainExtractor
+
+
+
+from langchain.prompts import PromptTemplate
+import os 
 from pprint import pprint 
+os.environ['ANONYMIZED_TELEMETRY']='false'
+
 
 class FinanceRAG:
     def __init__(
@@ -20,6 +34,8 @@ class FinanceRAG:
         llm_path,
         llm_hyperp,
     ):
+        self.device = 'cpu'
+
         self.logger = self.setup_logging()
 
         # Load synonym dictionary for company extraction
@@ -29,7 +45,7 @@ class FinanceRAG:
         self.load_sentence_embedding_model(smodel_path)
 
         # Load vectordb
-        self.load_vectordb(vectordb_path)
+        self.load_vectordb(vectordb_path, 'hit_QA_new')
 
         # Load query expansion LLM
         #self.load_query_expansion_llm(llm4qe_path)
@@ -44,8 +60,10 @@ class FinanceRAG:
         self.define_templates()
 
         #set pipeline 
-        self.set_pipeline()
+        self.set_compressor_pipline()
 
+        #set llm pipeline 
+        self.set_pipeline()
 
     def setup_logging(self):
         import logging
@@ -59,6 +77,27 @@ class FinanceRAG:
         
 
         return logger
+            
+    def extract_company(self, question):
+        comp = None
+        question_temp = ''.join(question.split())
+        for syn in self.syn2comp:
+            if syn in question_temp:
+                comp = self.syn2comp[syn]
+                break
+        return comp
+
+    
+    def set_compressor_pipline(self):
+        self.logger.info("set compressor pippline")
+        self.splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=0, separator=". ")
+        self.redundant_filter = EmbeddingsRedundantFilter(embeddings=self.smodel)
+        self.relevant_filter = EmbeddingsFilter(embeddings = self.smodel, similarity_threshold=0.3, k=2)
+        self.pipeline_compressor = DocumentCompressorPipeline(
+            transformers=[self.splitter, self.redundant_filter, self.relevant_filter]
+            
+        )
+
 
     def load_synonym_dictionary(self, synonym_dict_path):
         self.logger.info("Loading synonym dictionary...")
@@ -70,17 +109,19 @@ class FinanceRAG:
 
     def load_sentence_embedding_model(self, smodel_path):
         self.logger.info("Loading sentence embedding model...")
-        self.hf = HuggingFaceEmbeddings(
-            model_name=smodel_path, model_kwargs={"device": "cpu"}, encode_kwargs={"normalize_embeddings": False}
+        self.smodel = HuggingFaceEmbeddings(
+            model_name=smodel_path, model_kwargs={"device": self.device}, encode_kwargs={"normalize_embeddings": False}
         )
 
-    def load_vectordb(self, vectordb_path):
+
+    def load_vectordb(self, vectordb_path, collection_name):
         self.logger.info("Loading vectordb...")
+
         self.vectordb = Chroma(
-            collection_name='hit_QA_poc', 
-            persist_directory=vectordb_path, embedding_function=self.hf
+            collection_name=collection_name, 
+            persist_directory=vectordb_path, embedding_function=self.smodel
         )
-        self.logger.info("Loading vectordb...")
+        self.logger.info("Loading vectordb number of chunks : "+str(self.vectordb._collection.count()))
 
     def load_query_expansion_llm(self, llm4qe_path):
         self.logger.info("Loading query expansion LLM...")
@@ -127,18 +168,50 @@ class FinanceRAG:
             temperature=0.0001,
             eos_token_id=self.tokenizer.eos_token_id,
             pad_token_id=self.tokenizer.pad_token_id,
-            early_stopping=True,
             bad_words_ids=[[6], [13]],
         )
-        
-    def extract_company(self, question):
-        comp = None
-        question_temp = ''.join(question.split())
-        for syn in self.syn2comp:
-            if syn in question_temp:
-                comp = self.syn2comp[syn]
-                break
-        return comp
+
+
+    def _load_selfquery_retriever(self):
+        """
+        description : only retrieve chunks that has metadata related to the question 
+
+        In progress : json parsing error with huggingface llm models ?? the reason is because the retriever relys on the llm's quality 
+
+        ref : https://python.langchain.com/docs/modules/data_connection/retrievers/self_query/
+              https://github.com/langchain-ai/langchain/issues/5882
+        useage : 
+        docs = retriever.get_relevant_documents(question)
+
+        """
+        document_content_description = "애널리스트가 증권 거래소에서 발간하는 증권 리포트"
+
+        metadata_field_info = [
+                        AttributeInfo(
+                            name="company",
+                            description="회사명, 회사 이름, 주식회사",
+                            type="string",
+                        ),
+                        AttributeInfo(
+                            name="exchange",
+                            description="거래소, 증권거래소, 주식 거래소",
+                            type="string",
+                        ),
+                        AttributeInfo(
+                            name="source",
+                            description="PDF 파일 이름 ",
+                            type="string",
+                        ),
+                    ]
+
+
+        self.retriever = SelfQueryRetriever.from_llm(
+            self.llm_model,
+            self.vectordb,
+            document_content_description,
+            metadata_field_info,
+            verbose=True
+        )
 
     def query_expansion(self, question):
 
@@ -165,21 +238,27 @@ class FinanceRAG:
         return ''.join(generated.split('\n')[0].split(','))
 
 
-    def retrieve_generate(self, question, company):
+    def retrieve_generate(self, question, company, use_compressor=True, top_k_docs = 2):
         if company is None:
-            search_kwargs = {"k": 2}
+            search_kwargs = {"k": top_k_docs}
         elif company in self.company_list:
-            search_kwargs = {"k": 2} #{"filter": {"company": {"$eq": company}}, "k": 2}
+            search_kwargs = {"k": top_k_docs} #{"filter": {"company": {"$eq": company}}, "k": 2}
         else:
-            search_kwargs = {"k": 2}
+            search_kwargs = {"k": top_k_docs}
+
+        self.base_retriever = self.vectordb.as_retriever( search_type='similarity', search_kwargs = search_kwargs)
+
+        if use_compressor:
+            self.compressed_retriever = ContextualCompressionRetriever( base_compressor = self.pipeline_compressor, base_retriever = self.base_retriever, search_kwargs = search_kwargs)
+            retriever = self.compressed_retriever
+        else:
+            retriever = self.base_retriever
 
         with torch.no_grad():
             self.qa = RetrievalQA.from_chain_type(
                 llm=HuggingFacePipeline(pipeline=self.pipe),
                 chain_type="stuff",
-                retriever=self.vectordb.as_retriever(
-                    search_type="similarity", search_kwargs=search_kwargs
-                ),
+                retriever = retriever,
                 chain_type_kwargs={
                     "prompt": PromptTemplate(
                         template=self.template, input_variables=["context", "question"]
@@ -211,9 +290,9 @@ class FinanceRAG:
 
 if __name__ == '__main__':
 
-    dict_path = 'output_assets/주식명_동의어사전_new.xlsx'
+    dict_path = '/home/hanati/hit/output_assets/주식명_동의어사전_new.xlsx'
 
-    smodel_name = 'sentence_embedding_plm/ko-sbert-multitask'
+    smodel_name = '/home/hanati/hit/sentence_embedding_plm/ko-sbert-multitask'
 
     vectordb_path = '/home/hanati/hit/db_old'
 
